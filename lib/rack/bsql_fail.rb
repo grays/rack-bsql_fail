199b0f91ebd004155b2e7894070f19ed3db52aff
Jason L Perry <jasper@ambethia.com>
2009-12-22 15:08:56 -0500

    Cleanup

--- lib/rack/bsql_fail.rb
+++ lib/rack/bsql_fail.rb
@@ -1,14 +1,9 @@
 module Rack #:nodoc:
-  class BSQLFail < Struct.new :app, :options
-
-    def initialize app, routes
-      @app    = app
-      @routes = routes
-    end
+  class BSQLFail < Struct.new :app, :routes
 
     def call env
-      return @app.call(env) unless @routes.include?(env["PATH_INFO"])
-      status, headers, response = @app.call(env)
+      return app.call(env) unless routes.include?(env["PATH_INFO"])
+      status, headers, response = app.call(env)
       request = Rack::Request.new(env)
       if request.params.values.any? { |s| s =~ / and 1=[1,0]/ }
         response = 'GTFO'
